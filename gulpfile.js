c2b5b02676ca54d4fc50eabbd0627b3081306d14
Simon <simonxd@gmail.com>
2016-10-02 03:14:58 -0300

    1.0.0-beta

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 const gulp        = require('gulp');
+const gulpNodemon = require('gulp-nodemon');
 // tests
 const istanbul    = require('gulp-istanbul');
 const mocha       = require('gulp-mocha');
@@ -19,4 +20,20 @@ gulp.task('test', ['pre-test'], function () {
     .pipe(istanbul.writeReports())
     // Enforce a coverage of at least 90%
     .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+});
+
+gulp.task('nodemon', function () {
+  gulpNodemon({
+    script: 'cli/start.js',
+    env: {
+      RABBIT_MQ_URI: 'amqp://192.168.99.100',
+      DESTINATION_HOST_WHITELIST: 'habem.us',
+    },
+    ext: 'js',
+    ignore: [
+      'client/**/*',
+      'dist/**/*',
+      'gulpfile.js',
+    ],
+  })
 });
